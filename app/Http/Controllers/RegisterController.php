b32654ed9500f60ca1364d14bb4c54d5823e2b62
Oyeleye Festus Olatunbosun <oyeleyeolatunbosunfestus@gmail.com>
2023-04-27 01:20:49 +0100

    Store new user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\UserService;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterUserRequest;
 
 class RegisterController extends Controller
@@ -21,9 +22,17 @@ class RegisterController extends Controller
     public function store(RegisterUserRequest $request)
     {
         try {
+            $data = [
+                'name'      => $request->name,
+                'email'     => $request->email,
+                'password'  => Hash::make($request->password)
+            ];
 
+            $user = $this->userService->register($data);
+
+            return response()->json(['status' => 201, 'user' => $user], 201);
         } catch (\Exception $e) {
-            
+            return response()->json(['status' => 500, 'error' => $e->getMessage()], 500);
         }
     }
 }
