3e0c64b92b1e67f1708119b9377e10d3eceb1946
Oyeleye Festus Olatunbosun <oyeleyeolatunbosunfestus@gmail.com>
2023-04-28 01:44:59 +0100

    Added UserService, UserRepository, and UserRepositoryInterface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,13 @@
 namespace App\Providers;
 
 use App\Interfaces\UserRepositoryInterface;
+use App\Interfaces\PostRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use App\Repositories\UserRepository;
+use App\Repositories\PostRepository;
 use App\Services\UserService;
+use App\Services\PostService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,9 +21,13 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
-
         $this->app->bind(UserService::class, function ($app) {
             return new UserService($app->make(UserRepositoryInterface::class));
+        });
+
+        $this->app->bind(PostRepositoryInterface::class, PostRepository::class);
+        $this->app->bind(PostService::class, function ($app) {
+            return new PostService($app->make(PostRepositoryInterface::class));
         });
     }
 
